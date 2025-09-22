8e12b11cc8165b99b3ee5461aef710e00fc2af13
LEANDRO JUKEMURA <34816872+ljukemura@users.noreply.github.com>
2025-09-22 19:17:43 -0300

    Update vite.config.ts
    testes atira vit

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/",
+  base: mode === "development" ? "/" : "/kapoias-creative-hub/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
